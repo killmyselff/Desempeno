6deee1dcb7d9f560d89f62b081a8af38fb52457a
killmyselff <juangaleano857@gmail.com>
2025-05-08 15:12:31 -0500

    primera prueba de ejercicio

---
+++ src/ejercicio5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la contraseña: ");
+        String clave = scanner.nextLine();
+
+        if (clave.equals("java123")) {
+            System.out.println("Acceso concedido.");
+        } else {
+            System.out.println("Acceso denegado.");
+        }
+
+        scanner.close();
+    }
+}
