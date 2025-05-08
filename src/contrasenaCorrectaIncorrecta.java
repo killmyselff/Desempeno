6f32d1b54885340c237c1fc51541ad942b4b16bb
killmyselff <juangaleano857@gmail.com>
2025-05-08 15:17:55 -0500

    se actualiza y se finaliza el ejercicio

--- src/contrasenaCorrectaIncorrecta.java
+++ src/contrasenaCorrectaIncorrecta.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ejercicio5 {
+public class contrasenaCorrectaIncorrecta {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
